9b5e01d8aa95e3977939db916dc477699b1eac4b
Vadym <vvnechy@gmail.com>
2021-01-22 15:05:36 +0100

    title changes

--- routes/activity.js
+++ routes/activity.js
@@ -27,7 +27,7 @@ exports.execute = async (req, res) => {
         },
         values: {
           Event: data.inArguments[0].DropdownOptions,
-          Text: data.inArguments[0].Text,
+          Title: data.inArguments[0].Title,
         },
       },
     ]);
